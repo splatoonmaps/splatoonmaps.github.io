f02ec24d9fb2f51f959f6227bf52b04e341adb4a
splatoonmaps <splatoonmaps@users.noreply.github.com>
2016-04-08 22:55:13 +0200

    get that April data going

--- js/main.js
+++ js/main.js
@@ -99,6 +99,8 @@ $(function() {
               .append(buildGraph(data[i].splatzones.feb2016,data[i].towercontrol.feb2016,data[i].rainmaker.feb2016,"s"))
               .append('<div class="graph-name">Mar 2016</div>')
               .append(buildGraph(data[i].splatzones.mar2016,data[i].towercontrol.mar2016,data[i].rainmaker.mar2016,"s"))
+              .append('<div class="graph-name">Apr 2016</div>')
+              .append(buildGraph(data[i].splatzones.apr2016,data[i].towercontrol.apr2016,data[i].rainmaker.apr2016,"s"))
             )
           )
       )
